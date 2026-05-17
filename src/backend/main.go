20e98716c7de02ccb8ab2b4217f9f7131654bfeb
Frederico Samuel Halim <fredericosamuel01@gmail.com>
2026-05-17 19:52:57 +0700

    fix: allow all origins in CORS

--- src/backend/main.go
+++ src/backend/main.go
@@ -39,7 +39,7 @@ type Material struct {
 }
 
 func setCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 }
